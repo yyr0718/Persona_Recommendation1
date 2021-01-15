57a2a155b9c50e491390ffe153609aebf7b82ead
yyr0718 <76768222+yyr0718@users.noreply.github.com>
2021-01-15 10:30:35 +0800

    Create BiaoQianEntity.java

---
+++ java/com/nwsuaf/entity/BiaoQianEntity.java
@@ -0,0 +1,300 @@
+package com.nwsuaf.entity;
+
+public class BiaoQianEntity {
+	String cert_id;
+	String key1;
+	String key2;
+	String key3;
+	String key4;
+	String key5;
+	String key6;
+	String key7;
+	String key8;
+	String key9;
+	String key10;
+	double value1;
+	double value2;
+	double value3;
+	double value4;
+	double value5;
+	double value6;
+	double value7;
+	double value8;
+	double value9;
+	double value10;
+	
+	
+	
+	public double getSum() {
+		return (value1+value2+value3+value4+value5+value6+value7+value8+value9+value10);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "BiaoQianEntity [cert_id=" + cert_id + ", key1=" + key1 + ", key2=" + key2 + ", key3=" + key3 + ", key4="
+				+ key4 + ", key5=" + key5 + ", key6=" + key6 + ", key7=" + key7 + ", key8=" + key8 + ", key9=" + key9
+				+ ", key10=" + key10 + ", value1=" + value1 + ", value2=" + value2 + ", value3=" + value3 + ", value4="
+				+ value4 + ", value5=" + value5 + ", value6=" + value6 + ", value7=" + value7 + ", value8=" + value8
+				+ ", value9=" + value9 + ", value10=" + value10 + "]";
+	}
+
+
+
+	public String getCert_id() {
+		return cert_id;
+	}
+
+
+
+	public void setCert_id(String cert_id) {
+		this.cert_id = cert_id;
+	}
+
+
+
+	public String getKey1() {
+		return key1;
+	}
+
+
+
+	public void setKey1(String key1) {
+		this.key1 = key1;
+	}
+
+
+
+	public String getKey2() {
+		return key2;
+	}
+
+
+
+	public void setKey2(String key2) {
+		this.key2 = key2;
+	}
+
+
+
+	public String getKey3() {
+		return key3;
+	}
+
+
+
+	public void setKey3(String key3) {
+		this.key3 = key3;
+	}
+
+
+
+	public String getKey4() {
+		return key4;
+	}
+
+
+
+	public void setKey4(String key4) {
+		this.key4 = key4;
+	}
+
+
+
+	public String getKey5() {
+		return key5;
+	}
+
+
+
+	public void setKey5(String key5) {
+		this.key5 = key5;
+	}
+
+
+
+	public String getKey6() {
+		return key6;
+	}
+
+
+
+	public void setKey6(String key6) {
+		this.key6 = key6;
+	}
+
+
+
+	public String getKey7() {
+		return key7;
+	}
+
+
+
+	public void setKey7(String key7) {
+		this.key7 = key7;
+	}
+
+
+
+	public String getKey8() {
+		return key8;
+	}
+
+
+
+	public void setKey8(String key8) {
+		this.key8 = key8;
+	}
+
+
+
+	public String getKey9() {
+		return key9;
+	}
+
+
+
+	public void setKey9(String key9) {
+		this.key9 = key9;
+	}
+
+
+
+	public String getKey10() {
+		return key10;
+	}
+
+
+
+	public void setKey10(String key10) {
+		this.key10 = key10;
+	}
+
+
+
+	public double getValue1() {
+		return value1;
+	}
+
+
+
+	public void setValue1(double value1) {
+		this.value1 = value1;
+	}
+
+
+
+	public double getValue2() {
+		return value2;
+	}
+
+
+
+	public void setValue2(double value2) {
+		this.value2 = value2;
+	}
+
+
+
+	public double getValue3() {
+		return value3;
+	}
+
+
+
+	public void setValue3(double value3) {
+		this.value3 = value3;
+	}
+
+
+
+	public double getValue4() {
+		return value4;
+	}
+
+
+
+	public void setValue4(double value4) {
+		this.value4 = value4;
+	}
+
+
+
+	public double getValue5() {
+		return value5;
+	}
+
+
+
+	public void setValue5(double value5) {
+		this.value5 = value5;
+	}
+
+
+
+	public double getValue6() {
+		return value6;
+	}
+
+
+
+	public void setValue6(double value6) {
+		this.value6 = value6;
+	}
+
+
+
+	public double getValue7() {
+		return value7;
+	}
+
+
+
+	public void setValue7(double value7) {
+		this.value7 = value7;
+	}
+
+
+
+	public double getValue8() {
+		return value8;
+	}
+
+
+
+	public void setValue8(double value8) {
+		this.value8 = value8;
+	}
+
+
+
+	public double getValue9() {
+		return value9;
+	}
+
+
+
+	public void setValue9(double value9) {
+		this.value9 = value9;
+	}
+
+
+
+	public double getValue10() {
+		return value10;
+	}
+
+
+
+	public void setValue10(double value10) {
+		this.value10 = value10;
+	}
+	
+//	Map <String,Double> getMap() {
+//		Map <String,Double> myMap = new HashMap<String,Double>();
+//		return myMap;
+//	}
+
+}
