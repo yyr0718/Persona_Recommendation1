007c29859cda7e2dada9220fb25e7521a9cf4d67
yyr0718 <76768222+yyr0718@users.noreply.github.com>
2021-01-15 10:30:56 +0800

    Create LW.java

---
+++ java/com/nwsuaf/entity/LW.java
@@ -0,0 +1,138 @@
+package com.nwsuaf.entity;
+
+public class LW {
+	String subject;
+	String author;
+	String type;
+	String abstract1;
+	String download_num;
+	String quote_num;
+	String periodical;
+	String publish_time;
+	String keyword;
+	String key1;
+	String key2;
+	String key3;
+	String key4;
+	String key5;
+	String key6;
+	String key7;
+	String fund;
+	String unit;
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getAbstract1() {
+		return abstract1;
+	}
+	public void setAbstract1(String abstract1) {
+		this.abstract1 = abstract1;
+	}
+	public String getDownload_num() {
+		return download_num;
+	}
+	public void setDownload_num(String download_num) {
+		this.download_num = download_num;
+	}
+	public String getQuote_num() {
+		return quote_num;
+	}
+	public void setQuote_num(String quote_num) {
+		this.quote_num = quote_num;
+	}
+	public String getPeriodical() {
+		return periodical;
+	}
+	public void setPeriodical(String periodical) {
+		this.periodical = periodical;
+	}
+	public String getPublish_time() {
+		return publish_time;
+	}
+	public void setPublish_time(String publish_time) {
+		this.publish_time = publish_time;
+	}
+	public String getKeyWord() {
+		return keyword;
+	}
+	public void setKeyWord(String key) {
+		this.keyword = key;
+	}
+	public String getKey1() {
+		return key1;
+	}
+	public void setKey1(String key1) {
+		this.key1 = key1;
+	}
+	public String getKey2() {
+		return key2;
+	}
+	public void setKey2(String key2) {
+		this.key2 = key2;
+	}
+	public String getKey3() {
+		return key3;
+	}
+	public void setKey3(String key3) {
+		this.key3 = key3;
+	}
+	public String getKey4() {
+		return key4;
+	}
+	public void setKey4(String key4) {
+		this.key4 = key4;
+	}
+	public String getKey5() {
+		return key5;
+	}
+	public void setKey5(String key5) {
+		this.key5 = key5;
+	}
+	public String getKey6() {
+		return key6;
+	}
+	public void setKey6(String key6) {
+		this.key6 = key6;
+	}
+	public String getKey7() {
+		return key7;
+	}
+	public void setKey7(String key7) {
+		this.key7 = key7;
+	}
+	public String getFund() {
+		return fund;
+	}
+	public void setFund(String fund) {
+		this.fund = fund;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	@Override
+	public String toString() {
+		return "LW [subject=" + subject + ", author=" + author + ", type=" + type + ", abstract1=" + abstract1
+				+ ", download_num=" + download_num + ", quote_num=" + quote_num + ", periodical=" + periodical
+				+ ", publish_time=" + publish_time + ", key=" + keyword + ", key1=" + key1 + ", key2=" + key2 + ", key3="
+				+ key3 + ", key4=" + key4 + ", key5=" + key5 + ", key6=" + key6 + ", key7=" + key7 + ", fund=" + fund
+				+ ", unit=" + unit + "]";
+	}
+}
