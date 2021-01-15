f81446361b36b4d1300c67c26aeaa585f84395ed
yyr0718 <76768222+yyr0718@users.noreply.github.com>
2021-01-15 10:30:44 +0800

    Create CiYun.java

---
+++ java/com/nwsuaf/entity/CiYun.java
@@ -0,0 +1,25 @@
+package com.nwsuaf.entity;
+
+public class CiYun {
+	String key;
+	Double value;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public Double getValue() {
+		return value;
+	}
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	@Override
+	public String toString() {
+		return "CiYun [key=" + key + ", value=" + value + "]";
+	}
+	
+	
+
+}
