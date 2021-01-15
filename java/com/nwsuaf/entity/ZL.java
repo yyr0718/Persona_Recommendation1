592923b38d12c13fabdd30a419cb4b1778d26987
yyr0718 <76768222+yyr0718@users.noreply.github.com>
2021-01-15 10:31:14 +0800

    Create ZL.java

---
+++ java/com/nwsuaf/entity/ZL.java
@@ -0,0 +1,103 @@
+package com.nwsuaf.entity;
+
+public class ZL {
+
+	String sqnum;
+	String title;
+	String sq_day;
+	String gknum;
+	String gk_day;
+	String writer;
+	String dljg;
+	String dlr;
+	String summary;
+	String zqx;
+
+	@Override
+	public String toString() {
+		return "ZL [sqnum=" + sqnum + ", title=" + title + ", sq_day=" + sq_day + ", gknum=" + gknum + ", gk_day="
+				+ gk_day + ", writer=" + writer + ", dljg=" + dljg + ", dlr=" + dlr + ", summary=" + summary + ", zqx="
+				+ zqx + "]";
+	}
+
+	public String getSqnum() {
+		return sqnum;
+	}
+
+	public void setSqnum(String sqnum) {
+		this.sqnum = sqnum;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSq_day() {
+		return sq_day;
+	}
+
+	public void setSq_day(String sq_day) {
+		this.sq_day = sq_day;
+	}
+
+	public String getGknum() {
+		return gknum;
+	}
+
+	public void setGknum(String gknum) {
+		this.gknum = gknum;
+	}
+
+	public String getGk_day() {
+		return gk_day;
+	}
+
+	public void setGk_day(String gk_day) {
+		this.gk_day = gk_day;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getDljg() {
+		return dljg;
+	}
+
+	public void setDljg(String dljg) {
+		this.dljg = dljg;
+	}
+
+	public String getDlr() {
+		return dlr;
+	}
+
+	public void setDlr(String dlr) {
+		this.dlr = dlr;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getZqx() {
+		return zqx;
+	}
+
+	public void setZqx(String zqx) {
+		this.zqx = zqx;
+	}
+
+}
